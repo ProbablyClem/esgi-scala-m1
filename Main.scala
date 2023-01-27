c2ceb5057b6271c8f2eaf8cde613fbd209589d09
yorgoA <yorgoaoun5@hotmail.com>
2023-01-27 10:39:16 +0100

    Update ADD part Yorgo

--- Main.scala
+++ Main.scala
@@ -1,5 +1,9 @@
 import scala.collection.immutable.ArraySeq
 
+package commands
+
+
+
 /**
  * Main app containg program loop
  */
@@ -32,9 +36,9 @@ object Main extends App {
     }
 
     if (status.exit) {
-      status 
+      status
     } else {
-      run(newCanvas)  
+      run(newCanvas)
     }
   }
 
@@ -42,23 +46,66 @@ object Main extends App {
    * Execute various actions depending on an action command and optionnaly a Canvas
    */
   def execute(action: Seq[String], canvas: Canvas): (Canvas, Status) = {
-    try{
-      val execution: (Seq[String], Canvas) => (Canvas, Status) = action.head match {
+    val execution: (Seq[String], Canvas) => (Canvas, Status) = action.head match {
       case "exit" => Canvas.exit
       case "dummy" => Canvas.dummy
       case "dummy2" => Canvas.dummy2
-      case "new_canvas" => Canvas.newCanvas 
+      case "new_canvas" => Canvas.newCanvas
       // TODO: Add command here
+      case "load_image"=> Canvas.load_image
       case _ => Canvas.default
+
     }
 
     execution(action.tail, canvas)
-    }
-    catch {
-      case e: NumberFormatException => (canvas, Status(error = true, message = "Les arguments doivent être des entiers"))
-    }
   }
 }
+
+case class Triangle () extends Polygon ():
+
+  private val triangle = flawf ("Triangle")
+
+  def addPoints (x: VectorD, y: VectorD): Unit =
+    if x.dim != 3 || y.dim != 3 { triangle ("addPoints", "need exactly 3 vertices to make a triangle")}
+    for i <- x.indices do addPoint (x(i).toInt, y(i).toInt)
+  end addPoints
+  def setFrame (tx: Double, ty: Double, w: Double, h: Double): Unit =
+    val x = VectorD (0, 0, w) + tx
+    val y = VectorD (0, h, h) + ty
+    addPoints (x, y)
+  end setFrame
+
+end Triangle
+
+
+type PolygonShapeRectangular = RectangularShape | Polygon
+
+extension (s: PolygonShapeRectangular)
+  def CenterOfXIs(): Double =
+    s match
+      case _: RectangularShape => s.asInstanceOf [RectangularShape].getCenterX ()
+      case _: Polygon => s.asInstanceOf [Polygon].getBounds2D ().getCenterX ()
+  end CenterOfXIs
+  def CenterOfYIs(): Double =
+    s match
+      case _: RectangularShape => s.asInstanceOf [RectangularShape].getCenterY ()
+      case _: Polygon => s.asInstanceOf [Polygon].getBounds2D ().getCenterY ()
+  end CenterOfYIs
+  def WidthIs(): Double =
+    s match
+      case _: RectangularShape => s.asInstanceOf [RectangularShape].getWidth ()
+      case _: Polygon => s.asInstanceOf [Polygon].getBounds2D ().getWidth ()
+  end WidthIs
+  def HeightIs(): Double =
+    s match
+      case _: RectangularShape => s.asInstanceOf [RectangularShape].getHeight ()
+      case _: Polygon => s.asInstanceOf [Polygon].getBounds2D ().getHeight ()
+  end HeightIs
+  def setFrame (x: Double, y: Double, w: Double, h: Double): Unit =
+    s match
+      case _: RectangularShape => s.asInstanceOf [RectangularShape].setFrame (x, y, w, h)
+      case _: Polygon => s.asInstanceOf [Polygon].setFrame (x, y, w, h)
+  end setFrame
 
 /**
  * Define the status of the previous execution
@@ -68,6 +115,12 @@ case class Status(
   error: Boolean = false,
   message: String = ""
 )
+
+case class DrawRectangle(x1: Int, y1: Int, x2: Int, y2: Int) extends Command
+
+case class BucketFill(x: Int, y: Int, filler: Char) extends Command
+
+case class DrawLine(x1: Int, y1: Int, x2: Int, y2: Int) extends Command
 
 /**
  * A pixel is defined by its coordinates along with its color as a char
@@ -93,7 +146,7 @@ object Pixel {
   }
 
   /**
-   * Create a Pixel from a string "x,y" and a color 
+   * Create a Pixel from a string "x,y" and a color
    */
   def apply(s: String, color: Char): Pixel = {
    val coordinates = s.split(',')
@@ -112,7 +165,7 @@ case class Canvas(width: Int = 0, height: Int = 0, pixels: Vector[Vector[Pixel]]
    * Print the canvas in the console
    */
   def display: Unit = {
-    
+
     if (pixels.size == 0) {
       println("Empty Canvas")
     } else {
@@ -131,7 +184,7 @@ case class Canvas(width: Int = 0, height: Int = 0, pixels: Vector[Vector[Pixel]]
    * in the right position with its color
    */
   def update(pixel: Pixel): Canvas = {
-    
+
     val newPixels = pixels // TODO - Update pixels
 
     this.copy(pixels = newPixels)
@@ -154,20 +207,20 @@ object Canvas {
   /**
    * Exit execution
    */
-  def exit(arguments: Seq[String], canvas: Canvas): (Canvas, Status) = 
+  def exit(arguments: Seq[String], canvas: Canvas): (Canvas, Status) =
     (canvas, Status(exit = true, message="Received exit signal"))
 
   /**
    * Default execution for unknown action
    */
-  def default(arguments: Seq[String], canvas: Canvas): (Canvas, Status) = 
+  def default(arguments: Seq[String], canvas: Canvas): (Canvas, Status) =
     (canvas, Status(error = true, message = s"Unknown command"))
 
   /**
    * Create a static Canvas
    */
-  def dummy(arguments: Seq[String], canvas: Canvas): (Canvas, Status) = 
-    if (arguments.size > 0) 
+  def dummy(arguments: Seq[String], canvas: Canvas): (Canvas, Status) =
+    if (arguments.size > 0)
       (canvas, Status(error = true, message = "dummy action does not excpect arguments"))
     else  {
       val dummyCanvas = Canvas(
@@ -180,16 +233,27 @@ object Canvas {
           Vector(Pixel(0, 3, '#'), Pixel(1, 3, '.'), Pixel(2, 3, '#'))
         )
       )
-      
+
       (dummyCanvas, Status())
     }
+
+
+
+  /**
+   * Create canvas from image load
+   */
+  def load_image(arguments: Seq[String], canvas: Canvas): (Canvas, Status) =
+    val happy:Vector[String] =Source.fromFile("my_file").getLines().toVector
+      (load_imageCanvas, Status())
+    }
+
 
   /**
    * Create a static canvas using the Pixel companion object
    */
-  def dummy2(arguments: Seq[String], canvas: Canvas): (Canvas, Status) = 
-    if (arguments.size > 0) 
-      (canvas, Status(error = true, message = "dummy action does not expect arguments"))
+  def dummy2(arguments: Seq[String], canvas: Canvas): (Canvas, Status) =
+    if (arguments.size > 0)
+      (canvas, Status(error = true, message = "dummy action does not excpect arguments"))
     else  {
       val dummyCanvas = Canvas(
         width = 3,
@@ -198,15 +262,15 @@ object Canvas {
           Vector(Pixel("0,0", '#'), Pixel("1,0"), Pixel("2,0", '#')),
         )
       )
-      
+
       (dummyCanvas, Status())
     }
 
     /**
    * Create a static Canvas
    */
-  def newCanvas(arguments: Seq[String], canvas: Canvas): (Canvas, Status) = 
-    if (arguments.size !=3) 
+  def newCanvas(arguments: Seq[String], canvas: Canvas): (Canvas, Status) =
+    if (arguments.size !=3)
       (canvas, Status(error = true, message = "action newCanvas expects 3 arguments"))
     else  {
       val width = arguments(0).toInt
@@ -214,7 +278,7 @@ object Canvas {
       val color = arguments(2).charAt(0)
 
       var pixels = (0 until height).map(y => (0 until width).map(x => Pixel(x, y, color )).toVector).toVector
-      
+
       val dummyCanvas = Canvas(
         width = width,
         height = height,
